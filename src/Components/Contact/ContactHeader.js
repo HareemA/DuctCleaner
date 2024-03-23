ebf8ebe0de49cd8099596d00307ebfade5c0f26a
hadiashafqat <81703234+hadiashafqat@users.noreply.github.com>
2024-03-23 21:51:40 +0500

    changes contact header, added animation

--- src/Components/Contact/ContactHeader.js
+++ src/Components/Contact/ContactHeader.js
@@ -3,9 +3,28 @@ import headerpic from '../../Images/Contact-us.jpg';
 
 export default function ContactHeader() {
     return(
-        <div> 
-            <img src={headerpic} className="servicesImg"/>
-            <h1 className="servives-heading">Contact Us</h1>
+        <div>
+        <div className="contact--banner--container">
+            <h1 className="contact--banner--heading">CONTACT US: 
+                <div className="roller">
+                    <span id="rolltext">SEND YOUR<br/>
+                    QUERIES<br/>
+                    IN<br/>
+                    EMAIL<br/>
+                    </span>
+                </div>
+            </h1>
+        </div>
+            <h1 className="servives-heading">EMAIL US</h1>
         </div>
     )
 }
+
+
+
+
+
+
+
+
+        
