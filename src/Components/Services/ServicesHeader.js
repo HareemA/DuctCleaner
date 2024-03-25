864160116539f5a8e001d200ccc27ed16be6645e
HareemA <ahareem134@gmail.com>
2024-03-25 20:32:29 +0500

    Some changed

--- src/Components/Services/ServicesHeader.js
+++ src/Components/Services/ServicesHeader.js
@@ -3,7 +3,7 @@ import service from '../../Images/men-working-with-equipment-full-shot.jpg';
 
 export default function ServicesHeader() {
     return(
-        <div className="main--image">
+        <div className="main--image--services">
             <div className="header--container">
                 <h1><span>SERVICES</span></h1>
                 <span className="cen">Our Services</span>
