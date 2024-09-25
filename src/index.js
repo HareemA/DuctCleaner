8c366ceb3719897854ae0e58db6446efd11c0464
Hareem Ahmed <83877231+HareemA@users.noreply.github.com>
2024-09-25 13:03:25 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App} from './App';
 import BookPage from './Components/BookNow/BookPage';
 import ServicesPage from './Components/Services/ServicesPage';
 import ContactPage from './Components/Contact/ContactPage';
